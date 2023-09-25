469235cd0708244eb370a144e1dab5500eb7290c
bogdanmadelyk <bogdanmadelykkursach@gmail.com>
2023-09-25 15:47:48 +0300

    feat: add contains-duplicate problem

---
+++ src/problems/easy/contains-duplicate/contains-duplicate.test.ts
@@ -0,0 +1,13 @@
+import {containsDuplicate} from './contains-duplicate.ts';
+
+describe('Contains Duplicate tests', () => {
+	test('true case with 2 duplicate numbers', () => {
+		expect(containsDuplicate([1,2,3,1])).toBe(true)
+	})
+	test('true case with many duplicate numbers', () => {
+		expect(containsDuplicate([1,1,1,3,3,4,3,2,4,2])).toBe(true)
+	})
+	test('false case', () => {
+		expect(containsDuplicate([1,2,3,4])).toBe(false)
+	})
+})
---
+++ src/problems/easy/contains-duplicate/contains-duplicate.ts
@@ -0,0 +1,12 @@
+export const containsDuplicate = (nums: number[]): boolean => {
+	let map = new Map()
+	for (let i = 0; i < nums.length; i++) {
+		if(map.has(nums[i])) {
+			return true
+		}
+		else {
+			map.set(nums[i], true)
+		}
+	}
+	return false;
+};
