d97464d79b4b9829e2312e67c47461ae5f4e4e96
bogdanmadelyk <bogdanmadelykkursach@gmail.com>
2023-09-25 15:35:33 +0300

    feat: add valid-anagram problem

---
+++ src/problems/easy/valid-anagram/valid-anagram.test.ts
@@ -0,0 +1,10 @@
+import {isAnagram} from './valid-anagram.ts';
+
+describe('Valid Anagram tests', () => {
+	test('true case', () => {
+		expect(isAnagram('anagram', 'nagaram')).toBe(true);
+	});
+	test('false case', () => {
+		expect(isAnagram('rat', 'cat')).toBe(false);
+	})
+});
